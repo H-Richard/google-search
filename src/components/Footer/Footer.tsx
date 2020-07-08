7f875b7d7f149347876170108a12084dff6ebec4
Richard <r.hong@pm.me>
2020-07-08 04:32:47 -0400

    Feature/footer (#5)
    * feat(footer): add footer component
    
    * chore(index): update index meta

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import styles from './Footer.module.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <div className={styles.footer}>
+      <p> &copy; 2020 Richard Hong, all rights reserved.</p>
+      <p>
+        Proudly built with React, Typescript and{' '}
+        <span role="img" aria-label="heart">
+          ❤️.
+        </span>{' '}
+        <a
+          href="https://github.com/H-Richard/google-search"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source Code
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
