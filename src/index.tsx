ee0fd7d4afebb8d72ee7357307a77f00e5ce8921
Richard <r.hong@pm.me>
2020-07-09 00:04:28 -0400

    feat: add google analytics (#9)

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import Analytics from 'react-router-ga';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Analytics id="UA-172132321-1" debug>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Analytics>
   </BrowserRouter>,
   document.getElementById('root')
 );
